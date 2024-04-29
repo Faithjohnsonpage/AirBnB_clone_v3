534f49d32dfe0905fbedde1ffed5f084da98bc3e
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-04-29 15:23:04 +0100

    Fixed a bug

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,7 @@
 from flask import Blueprint
 
 # Create a Blueprint object for v1 views
-app_views = Blueprint('views', __name__, url_prefix='/api/v1')
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views.states import *
