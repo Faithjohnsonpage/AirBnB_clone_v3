1cc3de8109c30254d7e168abf70461c0def7cd87
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-05-02 19:23:20 +0100

    Corrected an error

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Flask application for retrieving statistics about various objects."""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
